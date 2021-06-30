6421ecb35db27502145c001f5bc67bcab3fe0207
Eliel Camara <eliel.camara@etec.sp.gov.br>
2021-06-30 16:14:59 -0300

    1.5.1

--- src/apiReact/banco.sql
+++ src/apiReact/banco.sql
@@ -9,3 +9,8 @@ password_user varchar(200) not null,
 cpf_user varchar(11) not null
 )engine=innodb;
 select *  from users;
+create table questions(
+id_question int not null auto_increment primary key,
+email_question varchar(60) not null,
+question varchar(1000) not null
+)engine=innodb;
